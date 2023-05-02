93a4a3a8c533db5ef77f273e5588efb622671f69
Jesse Sivonen <j.sivonen@hotmail.com>
2023-05-02 19:41:15 +0300

    Opt-in to the new Chrome headless mode

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 module.exports = {
   launch: {
-    headless: process.env.E2E_DEBUG !== "1",
+    headless: process.env.E2E_DEBUG !== "1" ? "new" : false,
     devtools: process.env.E2E_DEBUG === "1",
   },
   runBeforeUnloadOnClose: true,
